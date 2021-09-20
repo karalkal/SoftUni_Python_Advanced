0858cfa2e8d008a01acabf75f47fef1e79cb329d
karalkal <karalkal@yahoo.co.uk>
2021-09-20 21:20:48 +0100

    pre-lecture solution of task_4

---
+++ 03_exercise-stacks_queues_tuples_sets/4_honey(85%).py
@@ -0,0 +1,37 @@
+from collections import deque
+
+bees_queue = deque([int(x) for x in input().split()])
+nectar_stack = deque([int(x) for x in input().split()])
+operations_queue = deque(input().split())
+
+collected_queue = deque()
+total_honey = 0
+
+while nectar_stack and bees_queue:
+    bee = bees_queue[0]
+    nectar = nectar_stack.pop()
+    if bee <= nectar:
+        bee = bees_queue.popleft()
+        values_tuple = (bee, nectar)
+        collected_queue.append(values_tuple)
+
+while collected_queue:
+    current_op = operations_queue.popleft()
+    unpacked_values = collected_queue.popleft()
+    this_bee, its_load = unpacked_values
+
+    if current_op == "+":
+        total_honey += abs(this_bee + its_load)
+    elif current_op == "-":
+        total_honey += abs(this_bee - its_load)
+    elif current_op == "*":
+        total_honey += abs(this_bee * its_load)
+    elif current_op == "/" and its_load != 0:
+        total_honey += abs(int(this_bee / its_load))
+print(f"Total honey made: {total_honey}")
+if bees_queue:
+    print("Bees left: ", end="")
+    print(*bees_queue, sep=", ")
+if nectar_stack:
+    print("Nectar left: ", end="")
+    print(*nectar_stack, sep=", ")
