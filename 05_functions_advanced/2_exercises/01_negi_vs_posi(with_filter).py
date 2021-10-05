8d6de5a49d453faf6feb337f51db047625b071ad
karalkal <karalkal@yahoo.co.uk>
2021-10-05 05:08:20 +0100

    added solution to task 1

---
+++ 05_functions_advanced/2_exercises/01_negi_vs_posi(with_filter).py
@@ -0,0 +1,18 @@
+def calculate_posi_and_negi_sums(numbers_list):
+    negi_sum, posi_sum, stronger, weaker = 0, 0, "", ""
+    negi_sum = sum(filter(lambda x: x < 1, numbers_list))
+    posi_sum = sum(filter(lambda x: x > 1, numbers_list))
+
+    if abs(negi_sum) > posi_sum:
+        stronger, weaker = "negatives", "positives"
+    else:
+        stronger, weaker = "positives", "negatives"
+
+    return negi_sum, posi_sum, stronger, weaker
+
+
+numbers_list = [int(x) for x in input().split()]
+posi_and_negi_sums = calculate_posi_and_negi_sums(numbers_list)
+print(posi_and_negi_sums[0])
+print(posi_and_negi_sums[1])
+print(f"The {posi_and_negi_sums[2]} are stronger than the {posi_and_negi_sums[3]}")
